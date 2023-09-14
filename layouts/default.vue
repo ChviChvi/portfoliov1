da4cbc3dd60e8380c2559d37d0ca8a0b311d3293
Christiaan Vink <Christiaan_Vink@outlook.com>
2023-09-14 22:51:38 +0200

    good try! saving.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="background_background1">
+  <div class="background_background1" id="bugarea1">
+
+    <!-- Bug 1 -->
+    <div class="bug" id="bug1">
+      <img src="../assets/bugs/bug-example1.png" alt="Bug 1" />
+    </div>
+
+    <!-- Bug 2 -->
+    <div class="bug" id="bug2">
+      <img src="../assets/bugs/bug-example2.png" alt="Bug 2" />
+    </div>
+
 
     <header> n o o b</header>
     <div className="nav-background" />
@@ -28,6 +39,13 @@ export default {
   mounted() {
     // Add an event listener for the wheel event
     window.addEventListener('wheel', this.handleWheel);
+
+    setInterval(() => {
+      this.moveBug("bug1",50,50);
+      this.moveBug("bug2",50,50);
+    }, 1000);
+
+
   },
   beforeDestroy() {
     // Remove the event listener when the component is destroyed
@@ -46,49 +64,65 @@ export default {
       currentRouteIndex: 0, // Index of the current route in the routes array
     };
   },
-  // beforeDestroy() {
-  //   window.removeEventListener("scroll", this.handleScroll);
-  // },
-  // data() {
-  //   return {
-  //     scrollPosition: 0,
-  //     routes: [
-  //       "/",
-  //       "/about",
-  //       "/experiences",
-  //       "/projects",
-  //       "/courses",
-  //       "/contact",
-  //     ],
-  //   };
-  // },
+
   methods: {
-    // handleScroll() {
-    //   const scrollY = window.scrollY;
-    //   const scrollDirection = scrollY > this.scrollPosition ? "down" : "up";
-    //
-    //   console.log(scrollDirection)
-    //   // Update the scroll position
-    //   this.scrollPosition = scrollY;
-    //
-    //   // Define a threshold for when to trigger navigation
-    //   const threshold = 100; // Adjust this value as needed
-    //
-    //   // Get the current route index
-    //   const currentRouteIndex = this.routes.indexOf(this.$route.path);
-    //
-    //   if (scrollDirection === "down" && scrollY > threshold) {
-    //     // Scroll down, navigate to the next page if not on the last page
-    //     if (currentRouteIndex < this.routes.length - 1) {
-    //       this.$router.push(this.routes[currentRouteIndex + 1]);
-    //     }
-    //   } else if (scrollDirection === "up" && scrollY < threshold) {
-    //     // Scroll up, navigate to the previous page if not on the homepage
-    //     if (currentRouteIndex > 0) {
-    //       this.$router.push(this.routes[currentRouteIndex - 1]);
-    //     }
-    //   }
-    // },
+
+    moveBug1(elementId, duration) {
+      const element = document.getElementById(elementId);
+      if (element) {
+        // Add the "rotating" class to start the rotation animation
+        element.style.animation = `rotate ${duration}s linear `;
+       }
+     },
+
+
+    moveBug(bugId, bugWidth, bugHeight) {
+      const element = document.getElementById(bugId);
+      const maxDistance = 500;
+      const randomX = (Math.random() * 2 - 1) * maxDistance;
+      const randomY = (Math.random() * 2 - 1) * maxDistance;
+
+      // Get the current position
+      const currentTransform = getComputedStyle(element).transform;
+      const currentX = parseFloat(currentTransform.split(",")[4]) || 0;
+      const currentY = parseFloat(currentTransform.split(",")[5]) || 0;
+
+      // Calculate the new position relative to the current position
+      const newX = currentX + randomX;
+      const newY = currentY + randomY;
+
+      // Get the dimensions of the container
+      const container = document.getElementById("bugarea1"); // Replace with your actual container ID
+      const containerWidth = container.offsetWidth;
+      const containerHeight = container.offsetHeight;
+
+      // Ensure the new position is within the container boundaries
+      const padding = 200;
+
+      const clampedX = Math.min(Math.max(newX, padding), containerWidth - element.offsetWidth - padding);
+      const clampedY = Math.min(Math.max(newY, padding), containerHeight - element.offsetHeight - padding);
+
+      // Calculate the angle between the current position and the destination point
+      const angle = Math.atan2(clampedY - currentY, clampedX - currentX);
+      const angleDegrees = (angle * 180) / Math.PI;
+
+      // Apply the rotation and position
+      element.style.transform = `translate(${clampedX}px, ${clampedY}px) rotate(${angleDegrees}deg)`;
+      element.style.width = `${bugWidth}px`;
+      element.style.height = `${bugHeight}px`;
+
+
+      console.log(`Current position for bugId ${bugId}: X=${clampedX}, Y=${clampedY}`);
+    },
+
+
+
+
+
+
+
+
+
     handleWheel(event) {
       if (this.isScrolling) {
         // If scrolling action is already in progress, do nothing
